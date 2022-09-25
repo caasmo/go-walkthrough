2b9ffac26b0507d9ba46d02afdcc0eeeb7bb78e0
caasmo <226161+caasmo@users.noreply.github.com>
2022-09-25 23:18:44 +0200

    slice

--- slice.go
+++ slice.go
@@ -1,7 +1,10 @@
 package main
 
 // https://golangbot.com/arrays-and-slices/
-import "fmt"
+import (
+    "fmt"
+    "strings"
+)
 
 func main() {
 
@@ -9,48 +12,43 @@ func main() {
     var m [4]int
     m[0] = 1
     m1 := m[0]
-    fmt.Println(m1)
+	fmt.Printf("Type of the array: %T\n", m)
+    fmt.Println("Value of index 0 is", m1)
 
 	header("Slice literal")
     letters := []string{"a", "b", "c", "d"}
-    fmt.Println(letters)
+	fmt.Printf("Type of the array: %#v\n", letters)
 
-    // slice create make
 	header("Slice create with `make`")
     var s []byte
     s = make([]byte, 5, 5)
-    // s == []byte{0, 0, 0, 0, 0}
     fmt.Println(s)
 
-    // slicing
+	header("Slicing")
     c := []byte{'g', 'o', 'l', 'a', 'n', 'g'}
-    c1 := c[1:4] // []byte{'o', 'l', 'a'}, sharing the same storage as b
+    c1 := c[1:4] // []byte{'o', 'l', 'a'}
     fmt.Println(c1)
 
-    // copy
+	header("Copy")
     t := make([]byte, 5, 5)
     t1 := make([]byte, len(t)*2, (cap(t)+1)*2)
     copy(t1, t)
     fmt.Println(t1)
 
-    //
-    // append elements
-    //
+	header("Append elements")
     a := make([]int, 1)
     a = append(a, 1, 2, 3)
     // a == []int{0, 1, 2, 3}
     fmt.Println(a)
 
-    //
-    // append two slices
-    //
+	header("Append another slice")
     b1 := []string{"John", "Paul"}
     b2 := []string{"George", "Ringo", "Pete"}
     // a == []string{"John", "Paul", "George", "Ringo", "Pete"}
     b1 = append(b1, b2...)
     fmt.Println(b1)
 
-    // Example gist cryptowatch
+	header("Quiz")
     srcSlice := []int{1, 2, 3}
     anotherSlice := srcSlice[:3]
     for i := 0; i < 3; i++ {
